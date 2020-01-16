18c5a5acf33ea693fbde70cff6605b16cd485b28
Flavian <Flavianmohanraj@gmail.com>
2020-01-16 19:43:03 +0000

    refactor with callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,32 @@
 const request = require("request");
-
 const input = process.argv.slice(2);
 
-const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + input[0];
+const fetchBreedDescription = function(input, callback) {
+  const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + input[0];
+  request(URL, (error, response, body) => {
+    const data = JSON.parse(body);
 
+    if (response.statusCode === 404) {
+      callback("invalid URL", null);
+    } else if (data.length === 0) {
+      callback("breed not found", null);
+    } else {
+      callback(null, `${data[0].description}`);
+    }
+  });
+};
 
-request(URL, (error, response, body) => {
-  const data = JSON.parse(body);
-  console.log(data[0].description)
+module.exports = { fetchBreedDescription };
 
-  if (response.statusCode === 404) {
-    console.log("not found ");
-    return;
-  }
-  if (data.length === 0) {
-    console.log("breed not found");
-  }
-});
+// request(URL, (error, response, body) => {
+//   const data = JSON.parse(body);
+//   console.log(data[0].description)
+
+//   if (response.statusCode === 404) {
+//     console.log("not found ");
+//     return;
+//   }
+//   if (data.length === 0) {
+//     console.log("breed not found");
+//   }
+// });
