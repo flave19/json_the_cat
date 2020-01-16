105e0f47c0f8dec4c67ec75d98571e6680112f25
Flavian <Flavianmohanraj@gmail.com>
2020-01-16 18:47:57 +0000

    cleaner

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,26 +3,17 @@ const request = require("request");
 const input = process.argv.slice(2);
 
 const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + input[0];
-// const filePath = input[1]
 
-// console.log(response.statuscode())
 
 request(URL, (error, response, body) => {
   const data = JSON.parse(body);
-  // console.log(data[0].description)
-  console.log(response.statusCode);
+  console.log(data[0].description)
+
   if (response.statusCode === 404) {
     console.log("not found ");
-
     return;
   }
   if (data.length === 0) {
     console.log("breed not found");
   }
 });
-
-// console.log(typeof body)
-
-// const data = JSON.parse(body);
-// console.log(data);
-// console.log(typeof data);
